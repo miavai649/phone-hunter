56e9f5070d4c5151017ddfdbc8b38e3bf93d9921
Mahamudul Haque Noor <mahamudulhaquenoor@gmail.com>
2022-09-03 09:18:05 +0600

    show all btn added

--- js/app.js
+++ js/app.js
@@ -1,19 +1,26 @@
-const loadPhone = async (inputText) =>{
+const loadPhone = async (inputText, dataLimit) =>{
   const url = `https://openapi.programming-hero.com/api/phones?search=${inputText}`;
   const res = await fetch(url);
   const data = await res.json();
-  displayPhone(data.data);
+  displayPhone(data.data, dataLimit);
 }
 
-const displayPhone = phones =>{
+const displayPhone = (phones, dataLimit) =>{
   const phonesContainer = document.getElementById('phones-container');
   phonesContainer.textContent = '';
   // display maximum 20 phones
-  seachedPhones = phones.slice(0, 20);
+  const showAll = document.getElementById('show-all');
+  if(dataLimit && phones.length > 10){
+    phones = phones.slice(0, 10);
+    showAll.classList.remove('d-none')
+  }
+  else{
+    showAll.classList.add('d-none')
+  }
 
   // display no phones found
   const noFound = document.getElementById('no-found-msg');
-  if(seachedPhones.length === 0){
+  if(phones.length === 0){
     noFound.classList.remove('d-none')
   }
   else{
@@ -21,7 +28,7 @@ const displayPhone = phones =>{
   }
 
   // display all phones
-  seachedPhones.forEach(phone => {
+  phones.forEach(phone => {
     const phoneDiv = document.createElement('div');
     phoneDiv.classList.add('col')
     phoneDiv.innerHTML = `
@@ -30,6 +37,7 @@ const displayPhone = phones =>{
       <div class="card-body">
         <h5 class="card-title">${phone.phone_name}</h5>
         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary">Show Details</button>
       </div>
     </div>
     `
@@ -39,16 +47,21 @@ const displayPhone = phones =>{
   toggleSpinner(false);
 }
 
+const processSearch = (dataLimit) =>{
+   // start spinners
+   toggleSpinner(true);
+   const searchField = document.getElementById('search-field');
+   const searchText = searchField.value;
+  //  searchField.value = '';
+   loadPhone(searchText, dataLimit);
+}
+
 // handle search button click
 document.getElementById('search-btn').addEventListener('click', function(){
-  // start spinners
-  toggleSpinner(true);
-
-  const searchField = document.getElementById('search-field');
-  const searchText = searchField.value;
-  searchField.value = '';
-  loadPhone(searchText);
+  processSearch(10);
 })
+
+// search input field enter key handler
 
 const toggleSpinner = isLoading =>{
   const spinnerSection = document.getElementById('spinners');
@@ -60,4 +73,22 @@ const toggleSpinner = isLoading =>{
   }
 }
 
-// loadPhone();
+// not the best way to load show all
+document.getElementById('btn-show-all').addEventListener('click', function(){
+  processSearch();
+})
+
+const loadPhoneDetails = async id =>{
+  const url = `https://openapi.programming-hero.com/api/phone/${id}`
+  const res = await fetch(url);
+  const data = await res.json();
+  displayPhoneDetail(data.data);
+}
+
+const displayPhoneDetail = phone =>{
+  console.log(phone);
+  const modalTitle = document.getElementById('phoneDetailModalLabel')
+  modalTitle.innerText = phone.name;
+}
+
+loadPhone('apple');
